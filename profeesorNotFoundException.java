08d9fd01b4960ebdb82174d1170f9b4d554105c4
Rohila26 <kududhularohila@gmail.com>
2024-11-22 13:53:31 +0530

    Add files via upload

---
+++ profeesorNotFoundException.java
@@ -0,0 +1,9 @@
+package com.university.exception;
+
+public class profeesorNotFoundException extends Exception {
+	public  profeesorNotFoundException(String message) {
+		super(message);
+	}
+
+}
+ 
